e36effc580bebd8cd91dc454129b58c4c4c79e06
Vinit Miranda <vinit.miranda@gmail.com>
2017-11-17 05:11:44 +0800

    Up and Down done

--- app/src/main/java/jxvmiranda/guitar_composer/MainActivity.java
+++ app/src/main/java/jxvmiranda/guitar_composer/MainActivity.java
@@ -1,39 +1,41 @@
 package jxvmiranda.guitar_composer;
 
+import android.content.res.AssetFileDescriptor;
 import android.media.AudioAttributes;
 import android.media.AudioFormat;
 import android.media.AudioTrack;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
-    AudioTrack track;
     InputStream stream;
+    int resource;
+    MediaPlayer track;
+    boolean record;
     @RequiresApi(api = Build.VERSION_CODES.M)
     private void play(){
-        stream = getResources().openRawResource(R);
-        int minBufferSize = AudioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
-        track = new AudioTrack.Builder()
-                .setAudioAttributes(new AudioAttributes.Builder()
-                        .setUsage(AudioAttributes.USAGE_MEDIA)
-                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                        .build())
-                .setAudioFormat(new AudioFormat.Builder()
-                        .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-                        .setSampleRate(44100)
-                        .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
-                        .build())
-                .setTransferMode(AudioTrack.MODE_STREAM)
-                .setBufferSizeInBytes(minBufferSize)
-                .build();
+        track = MediaPlayer.create(this, resource);
+        track.start();
     }
+    private void setResource(){
 
+    }
+    private void record(){
+
+    }
+    long startTime;
+    boolean timerOn = false;
     public class UpOnClickListener implements View.OnClickListener{
         @RequiresApi(api = Build.VERSION_CODES.M)
         @Override
@@ -41,7 +43,19 @@ public class MainActivity extends AppCompatActivity {
             Runnable r = new Runnable(){
                 @Override
                 public void run() {
-
+                    resource = R.raw.test1;
+                    if (track != null) {
+                        track.reset();
+                    }
+                    if(!timerOn) {
+                        startTime = System.currentTimeMillis();
+                        timerOn = true;
+                    }
+                    else{
+                        Log.d("TAG", "difference in time " + Long.toString(System.currentTimeMillis() - startTime));
+                        timerOn = false;
+                    }
+                    play();
                 }
             };
             Thread t = new Thread(r);
@@ -55,6 +69,18 @@ public class MainActivity extends AppCompatActivity {
             Runnable r = new Runnable(){
                 @Override
                 public void run() {
+                    resource = R.raw.test2;
+                    if (track != null) {
+                        track.reset();
+                    }
+                    if(!timerOn) {
+                        startTime = System.currentTimeMillis();
+                        timerOn = true;
+                    }
+                    else{
+                        Log.d("TAG", "difference in time " + Long.toString(System.currentTimeMillis() - startTime));
+                        timerOn = false;
+                    }
                     play();
                 }
             };
@@ -66,6 +92,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button up = (Button) findViewById(R.id.up);
+        Button down = (Button) findViewById(R.id.down);
+        up.setOnClickListener(new UpOnClickListener());
+        down.setOnClickListener(new DownOnClickListener());
+        ToggleButton record_toggle = (ToggleButton) findViewById(R.id.record_toggle);
+        record_toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    record = true;
+                } else {
+                    record = false;
+                }
+            }
+        });
     }
+
+
 }
